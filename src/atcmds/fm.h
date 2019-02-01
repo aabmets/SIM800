8a4777f218c804e2a7e1247e0827ed73549604c8
Mortuus Medicus <mortuusmedicus@gmail.com>
2019-02-01 01:50:24 +0100

    Added fm.h

---
+++ src/atcmds/fm.h
@@ -0,0 +1,13 @@
+/*
+█████████████████████████████████████████████████████
+████   ATcmd         Manpage        API Method   ████   Count: 7 commands
+█████████████████████████████████████████████████████
+*/
+public:
+    /* AT+FMOPEN     6   */    void fmOpen                  (CmdType, char* = "");
+    /* AT+FMCLOSE    7   */    void fmClose                 (CmdType);
+    /* AT+FMFREQ     7   */    void fmSetFrequency          (CmdType, char* = "");
+    /* AT+FMVOLUME   7   */    void fmSetVolume             (CmdType, char* = "");
+    /* AT+FMSCAN     8   */    void fmAutoSearchChannel     (CmdType);
+    /* AT+FMSIGNAL   8   */    void fmQuerySignalLevel      (CmdType, char* = "");
+    /* AT+FMVALID    9   */    void fmCheckFrequencyValid   (CmdType, char* = "");
